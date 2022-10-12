c5467c10a70699c1dbd79c3aa2c3aa60a447f1ff
fishfei137 <kueiyufei137@gmail.com>
2022-10-13 01:33:28 +0800

    datajson

--- main_data/gephi_sigmajs/network/datajson.py
+++ main_data/gephi_sigmajs/network/datajson.py
@@ -1,16 +1,20 @@
 import json
-import pandas as pd
+
+# edge colour to follow target node colour
 
 with open('./main_data/gephi_sigmajs/network/data.json', 'r') as f:
     data = json.load(f)
-    
-colours = {"0": "rgb(249,119,67)", "1": "rgb(154,150,229)", "2": "rgb(40,179,106)"}
-modclass = {}
-for i in data["nodes"]:
-    modclass[i["id"]] = i["attributes"]["Modularity Class"]
 
-for i in data["edges"]:
-    i["color"] = colours[modclass[i["target"]]]
+colours = {}
+for node in data["nodes"]:
+    colours[node["attributes"]["Modularity Class"]] = node["color"]
+
+modclass = {}
+for node in data["nodes"]:
+    modclass[node["id"]] = node["attributes"]["Modularity Class"]
+
+for edge in data["edges"]:
+    edge["color"] = colours[modclass[edge["target"]]]
     
-with open('./main_data/gephi_sigmajs/network/data.json', 'w') as f:   # update video id
+with open('./main_data/gephi_sigmajs/network/data.json', 'w') as f:
     json.dump(data, f)
